da5ef7cf2f44b1227cbf5f69c82b38533dd54e20
Oleg Skiryuk <OSkiryuk@beeline.ru>
2021-07-25 15:58:51 +0300

    fixed bug

--- lib/app/modules/splash/pages/splash_page/splash_page.dart
+++ lib/app/modules/splash/pages/splash_page/splash_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:modular_router_outlet/app/modules/main_tabs/main_tabs_module.dart';
+import 'package:modular_router_outlet/app/modules/main_tabs/modules/home/home_module.dart';
 
 class SplashPage extends StatefulWidget {
   @override
@@ -11,7 +12,7 @@ class SplashPageState extends State<SplashPage> {
   @override
   void initState() {
     super.initState();
-    Modular.to.pushReplacementNamed(MainTabsModule.routeName);
+    Modular.to.navigate(MainTabsModule.routeName + HomeModule.routeName);
   }
 
   @override
